30222e83a64b4ece5fa010dc044268310a41061b
khalil NOUI <noui.khalil.pr@gmail.com>
2024-10-30 16:42:58 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "Kali is the champion"
+
+document.querySelector('body').append(newHeader)
+
